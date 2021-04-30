12dae01c42836c8a63483fe8921b8335cec767ca
Greg D'Amato <gregdamato2@gmail.com>
2021-04-30 10:08:41 -0400

    syntax fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,7 +32,7 @@ var fightOrSkip = function () {
     return true;
     }
   }
-}
+};
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
@@ -56,7 +56,7 @@ var fight = function(enemy) {
         console.log("playerInfo.money", playerInfo.money)
         break;
       }
-    }
+    };
 
     // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -173,24 +173,17 @@ var shop = function() {
 
   // use switch case to carry out action
   switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
+    case 3:
       window.alert('Leaving the store.');
-
-      // do nothing, so function will end
       break;
     default:
       window.alert('You did not pick a valid option. Try again.');
-
-      // call shop() again to force player to pick a valid option
       shop();
       break;
   }
